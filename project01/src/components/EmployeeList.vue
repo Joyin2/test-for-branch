912329090d8f2ef5db5aa8bcd661343adece6e26
Joyin2 <joyinlaskar2000@gmail.com>
2022-06-19 22:03:20 -0700

    new

---
+++ project01/src/components/EmployeeList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Employee List</h1>
+    </div>
+</template>
+<script>
+import Vue from 'vue';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+Vue.use(VueAxios, axios)
+export default {
+    name:"EmployeeList"
+}
+</script>
+<style scoped>
+
+</style>
